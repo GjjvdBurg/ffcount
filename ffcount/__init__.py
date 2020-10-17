4a5d796c943af026637c3417854723d6efabb837
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2020-10-17 20:41:43 +0200

    Add type annotations

--- ffcount/__init__.py
+++ ffcount/__init__.py
@@ -1,35 +1,43 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.5'
+__version__ = "0.1.5"
 
 # Author: Gertjan van den Burg <gertjanvandenburg@gmail.com>
 #
 # License: Apache License 2.0
 
+from typing import Tuple, Union
+
 from .count import fast_file_count
 
-def ffcount(path='.', recursive=True, hidden=True, quiet=True):
+
+def ffcount(
+    path: Union[str, bytes] = ".",
+    recursive: bool = True,
+    hidden: bool = True,
+    quiet: bool = True,
+) -> Tuple[int, int]:
     """Fast file count
 
-    Count the files and directories in the given path. By default the function 
-    is recursive and does not print errors. This function uses the C 
+    Count the files and directories in the given path. By default the function
+    is recursive and does not print errors. This function uses the C
     implementation by Christopher Schultz.
 
     Parameters
     ----------
     path : str or bytes
-        The path where to start counting. By default the current working 
+        The path where to start counting. By default the current working
         directory will be used.
 
     recursive : bool
-        To recurse or not to recurse. If recurse is False, only the files and 
+        To recurse or not to recurse. If recurse is False, only the files and
         directories in the directory given by ``path`` will be counted.
 
     hidden : bool
         Count hidden files and directories as well.
 
     quiet : bool
-        Don't print errors to the screen. If True, the function will fail 
+        Don't print errors to the screen. If True, the function will fail
         quietly and not print any errors.
 
     Returns
---
+++ ffcount/count.pyi
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Stub file for count.pyx
+#
+
+from typing import Tuple
+
+def fast_file_count(root: bytes, recursive : bool, hidden: bool, quiet:bool) -> Tuple[int, int]:
+    pass
+
+
