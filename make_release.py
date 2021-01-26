4f4c02a3c1dc7528c59f3b3f1bdb030fda0afe93
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-01-26 14:02:26 +0000

    Update release script

--- make_release.py
+++ make_release.py
@@ -15,10 +15,11 @@ Date: 2019-07-23
 import colorama
 import os
 import sys
+import tempfile
 import webbrowser
 
 URLS = {
-    "Travis": "https://travis-ci.org/GjjvdBurg/ffcount",
+    "CI": "https://github.com/GjjvdBurg/ffcount/actions",
 }
 
 
@@ -109,6 +110,12 @@ class UpdateChangelog(Step):
         self.print_run("vi CHANGELOG.md")
 
 
+class UpdateReadme(Step):
+    def action(self, context):
+        self.instruct("Update readme if necessary")
+        self.print_run("vi README.md")
+
+
 class RunTests(Step):
     def action(self, context):
         self.do_cmd("make test")
@@ -116,8 +123,8 @@ class RunTests(Step):
 
 class BumpVersionPackage(Step):
     def action(self, context):
-        self.instruct(f"Update __version__.py with new version")
-        self.print_run(f"vi {context['pkgname']}/__version__.py")
+        self.instruct("Update __version__.py with new version")
+        self.do_cmd(f"vi {context['pkgname']}/__version__.py")
 
     def post(self, context):
         wait_for_enter()
@@ -125,10 +132,7 @@ class BumpVersionPackage(Step):
 
     def _get_version(self, context):
         # Get the version from the version file
-        about = {}
-        with open(f"{context['pkgname'].lower()}/__version__.py", "r") as fp:
-            exec(fp.read(), about)
-        return about["__version__"]
+        return get_package_version(context["pkgname"])
 
 
 class MakeClean(Step):
@@ -136,33 +140,17 @@ class MakeClean(Step):
         self.do_cmd("make clean")
 
 
-class MakeDocs(Step):
-    def action(self, context):
-        self.do_cmd("make docs")
-
-
-class MakeDist(Step):
-    def action(self, context):
-        self.do_cmd("make dist")
-
-
-class PushToTestPyPI(Step):
-    def action(self, context):
-        self.do_cmd(
-            "twine upload --repository-url https://test.pypi.org/legacy/ dist/*"
-        )
-
-
 class InstallFromTestPyPI(Step):
     def action(self, context):
-        self.print_run("cd /tmp/")
-        self.print_cmd("rm -rf ./venv")
-        self.print_cmd("virtualenv ./venv")
-        self.print_cmd("source ./venv/bin/activate")
-        self.print_cmd(
-            "pip install --index-url https://test.pypi.org/simple/ "
-            + f"--extra-index-url https://pypi.org/simple {context['pkgname']}=={context['version']}"
+        tmpvenv = tempfile.mkdtemp(prefix="ffcount_venv_")
+        self.do_cmd(
+            f"python -m venv {tmpvenv} && source {tmpvenv}/bin/activate && "
+            "pip install --no-cache-dir --index-url "
+            "https://test.pypi.org/simple/ "
+            "--extra-index-url https://pypi.org/simple "
+            f"{context['pkgname']}=={context['version']}"
         )
+        context["tmpvenv"] = tmpvenv
 
 
 class TestPackage(Step):
@@ -170,13 +158,14 @@ class TestPackage(Step):
         self.instruct(
             f"Ensure that the following command gives version {context['version']}"
         )
-        self.print_run(f"{context['pkgname']} -V")
+        self.do_cmd(
+            f"source {context['tmpvenv']}/bin/activate && {context['pkgname']} -V"
+        )
 
 
-class DeactivateVenv(Step):
+class RemoveVenv(Step):
     def action(self, context):
-        self.print_run("deactivate")
-        self.instruct("Go back to the project directory")
+        self.do_cmd(f"rm -rf {context['tmpvenv']}")
 
 
 class GitTagVersion(Step):
@@ -199,6 +188,9 @@ class GitAdd(Step):
 class GitAddRelease(Step):
     def action(self, context):
         self.instruct("Add Changelog & Readme to git")
+        self.instruct(
+            f"Commit with title: {context['pkgname']} Release {context['version']}"
+        )
         self.instruct("Embed changelog in body commit message")
         self.print_run("git gui")
 
@@ -213,12 +205,10 @@ class PushToGitHub(Step):
         self.do_cmd("git push -u --tags origin master")
 
 
-class WaitForTravis(Step):
+class WaitForCI(Step):
     def action(self, context):
-        webbrowser.open(URLS["Travis"])
-        self.instruct(
-            "Wait for Travis to complete and verify that its successful"
-        )
+        webbrowser.open(URLS["CI"])
+        self.instruct("Wait for CI to complete and verify that its successful")
 
 
 def main(target=None):
@@ -227,29 +217,26 @@ def main(target=None):
         ("gittomaster", GitToMaster()),
         ("gitadd1", GitAdd()),
         ("clean1", MakeClean()),
-        ("test1", RunTests()),
-        # trigger Travis to run tests on all platforms
-        ("push1", PushToGitHub(),),
-        ("travis1", WaitForTravis()),
+        ("runtests", RunTests()),
+        # trigger CI to run tests on all platforms
+        ("push1", PushToGitHub()),
+        ("ci1", WaitForCI()),
         ("bumpversion", BumpVersionPackage()),
         ("gitadd2", GitAdd()),
-        ("pre", GitTagPreRelease()),
-        # trigger Travis to run tests using cibuildwheel
-        ("push2", PushToGitHub(),),
-        ("travis2", WaitForTravis()),
+        ("gittagpre", GitTagPreRelease()),
+        # trigger CI to run tests using cibuildwheel
+        ("push2", PushToGitHub()),
+        ("ci2", WaitForCI()),
         ("changelog", UpdateChangelog()),
-        ("clean2", MakeClean()),
-        ("dist", MakeDist()),
-        ("testpypi", PushToTestPyPI()),
+        ("readme", UpdateReadme()),
         ("install", InstallFromTestPyPI()),
-        ("test2", TestPackage()),
-        ("deactivate", DeactivateVenv()),
-        ("gitadd3", GitAddRelease()),
-        ("pypi", PushToPyPI()),
-        ("tag", GitTagVersion()),
-        # triggers Travis to build and push to PyPI
+        ("testpkg", TestPackage()),
+        ("remove_venv", RemoveVenv()),
+        ("addrelease", GitAddRelease()),
+        ("tagfinal", GitTagVersion()),
+        # triggers CI to build with cibw and push to PyPI
         ("push3", PushToGitHub()),
-        ("travis3", WaitForTravis()),
+        ("ci3", WaitForCI()),
     ]
     context = {}
     context["pkgname"] = get_package_name()
